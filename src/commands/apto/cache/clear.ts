2347aac4c536f2ec93c1ccdad351bf0c4ac9c143
Zack Whipkey <zack.whipkey@gmail.com>
2021-07-05 22:54:21 -0500

    initial prototype

--- src/commands/apto/cache/clear.ts
+++ src/commands/apto/cache/clear.ts
@@ -1,27 +1,23 @@
-/*
- * Copyright (c) 2020, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
 import { flags, SfdxCommand } from '@salesforce/command';
-import { Messages, SfdxError, Org } from '@salesforce/core';
+import { Messages, SfdxError, Org, fs } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
+import * as puppeteer from 'puppeteer';
+import * as cheerio from 'cheerio';
+
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
 
 // Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
 // or any library that is using the messages framework can also be loaded this way.
-const messages = Messages.loadMessages('zfdx', 'cache');
+const messages = Messages.loadMessages('apto', 'cache');
 
 export default class Clear extends SfdxCommand {
   public static description = messages.getMessage('commandDescription');
 
   public static examples = [
-    `$ sfdx cache:clear --targetusername myOrg@example.com
-Hello world! This is org: MyOrg and I will be around until Tue Mar 20 2018!
-My hub org id is: 00Dxx000000001234
+    `$ sfdx cache:clear --targetusername myOrg@example.com 
+    Default Partition:
   `,
     `$ sfdx hello:org --targetusername myOrg@example.com
 This is org: MyOrg and I will be around until Tue Mar 20 2018!
@@ -35,11 +31,7 @@ This is org: MyOrg and I will be around until Tue Mar 20 2018!
     name: flags.string({
       char: 'n',
       description: messages.getMessage('nameFlagDescription'),
-    }),
-    force: flags.boolean({
-      char: 'f',
-      description: messages.getMessage('forceFlagDescription'),
-    }),
+    })
   };
 
   // Comment this out if your command does not require an org username
@@ -51,6 +43,72 @@ This is org: MyOrg and I will be around until Tue Mar 20 2018!
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = false;
 
+  private static CLEAR_CACHE_BTN_SELECTOR = 'input[value="Clear Cache"]';
+
+  public async run(): Promise<AnyJson> {
+    let name = (this.flags.name) as string;
+    const partitionListURLPath = 'udd/PlatformCachePartition/listPlatformCachePartition.apexp';
+    const frontDoorUrl = (await this.buildFrontdoorUrl()) + `&retURL=${partitionListURLPath}`;
+
+    const browser = await puppeteer.launch({
+      args: ['--disable-web-security', '--disable-features=IsolateOrigins', ' --disable-site-isolation-trials'],
+    });
+    const context = browser.defaultBrowserContext();
+    await context.overridePermissions(frontDoorUrl, ['notifications']);
+    const page = await browser.newPage();
+
+    const headlessUserAgent = await page.evaluate(() => navigator.userAgent);
+    const chromeUserAgent = headlessUserAgent.replace('HeadlessChrome', 'Chrome');
+    await page.setUserAgent(chromeUserAgent);
+    const pageResult = await page.goto(frontDoorUrl, { waitUntil: 'networkidle2' });
+    
+    const pageText = await pageResult.text();
+    const $partitionList = cheerio.load(pageText);
+    const $partitions = $partitionList('tr .dataRow');
+    const partitions = [];
+    $partitions.each((index, ele) => {
+      const $partitionRow = $partitionList(ele);
+      const $partitionLink = $partitionList(($partitionRow.children().get(3)));
+      const $defaultCheckbox = $partitionRow.find('img.checkImg');
+      const isDefault = $defaultCheckbox.attr('alt') == 'Checked' ? true : false;
+      partitions.push({ name: $partitionLink.text(), href: $partitionLink.children('a').attr('href'), default : isDefault });
+    });
+
+    
+    const partition = name ? partitions.find(x => x.name == name) : partitions.find(x => x.default);
+    name = partition ? partition.name : name;
+    if (partition) {
+      const orgURL = new URL(frontDoorUrl);
+      const orgHost = orgURL.host;
+      
+      const partitionDetailPageResult = await page.goto(`https://${orgHost + partition.href}`, { waitUntil: 'networkidle2' });
+      page.on('dialog', async dialog => {
+        await dialog.accept();
+      });
+      try {
+        await partitionDetailPageResult.frame().click(Clear.CLEAR_CACHE_BTN_SELECTOR, {});
+      } catch (e) {
+        await browser.close();
+        const cacheEmptyMessage = messages.getMessage('cacheEmpty', [name]);
+        this.ux.warn(cacheEmptyMessage);
+        return { success: true, partitionName: name};
+
+      }
+
+      await page.waitForNavigation();
+    } else {
+      await browser.close();
+      throw new SfdxError(messages.getMessage('noPartitionFound', [name]));
+    }
+
+    await browser.close();
+    this.ux.log(
+      messages.getMessage('cacheClearSuccessful', [name])
+    );
+    // Return an object to be displayed with --json
+    return { success : true, partitionName: name };
+  }
+
   private async buildFrontdoorUrl(): Promise<string> {
     await this.org.refreshAuth(); // we need a live accessToken for the frontdoor url
     const conn = this.org.getConnection();
@@ -58,53 +116,5 @@ This is org: MyOrg and I will be around until Tue Mar 20 2018!
     const instanceUrl = this.org.getField(Org.Fields.INSTANCE_URL) as string;
     const instanceUrlClean = instanceUrl.replace(/\/$/, '');
     return `${instanceUrlClean}/secur/frontdoor.jsp?sid=${accessToken}`;
-  }
-
-  public async run(): Promise<AnyJson> {
-    const name = (this.flags.name || 'world') as string;
-    const frontDoorUrl = await this.buildFrontdoorUrl();
-    // this.org is guaranteed because requiresUsername=true, as opposed to supportsUsername
-    const conn = this.org.getConnection();
-    
-    const query = 'Select Name, TrialExpirationDate from Organization';
-
-    // The type we are querying for
-    interface Organization {
-      Name: string;
-      TrialExpirationDate: string;
-    }
-
-    // Query the org
-    const result = await conn.query<Organization>(query);
-
-    // Organization will always return one result, but this is an example of throwing an error
-    // The output and --json will automatically be handled for you.
-    if (!result.records || result.records.length <= 0) {
-      throw new SfdxError(messages.getMessage('errorNoOrgResults', [this.org.getOrgId()]));
-    }
-
-    // Organization always only returns one result
-    const orgName = result.records[0].Name;
-    const trialExpirationDate = result.records[0].TrialExpirationDate;
-
-    let outputString = `Hello ${name}! This is org: ${orgName}`;
-    if (trialExpirationDate) {
-      const date = new Date(trialExpirationDate).toDateString();
-      outputString = `${outputString} and I will be around until ${date}!`;
-    }
-    this.ux.log(outputString);
-
-    // this.hubOrg is NOT guaranteed because supportsHubOrgUsername=true, as opposed to requiresHubOrgUsername.
-    if (this.hubOrg) {
-      const hubOrgId = this.hubOrg.getOrgId();
-      this.ux.log(`My hub org id is: ${hubOrgId}`);
-    }
-
-    if (this.flags.force && this.args.file) {
-      this.ux.log(`You input --force and a file: ${this.args.file as string}`);
-    }
-
-    // Return an object to be displayed with --json
-    return { orgId: this.org.getOrgId(), outputString };
   }
 }
